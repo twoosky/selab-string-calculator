c693b7bde85492babec2f75ccdfa68b3fd7ba51e
leeheechan <coolzz7@naver.com>
2021-09-28 23:03:52 +0900

    build: project setting

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+    @Test
+    void test() {
+        assertEquals(2, 1 + 1);
+    }
+}
