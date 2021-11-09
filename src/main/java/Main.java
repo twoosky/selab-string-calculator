f152200b44c713aa86726d5a5ed010fdd7265050
kimsiyeon <pennya6@hs.ac.kr>
2021-11-10 00:49:04 +0900

    계산 부분 처리중

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,49 +1,63 @@
-import java.io.IOException;
-import java.util.InputMismatchException;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner s=new Scanner(System.in);
 
+        Queue<String> number=new LinkedList<>();
+        Queue<String> word=new LinkedList<>();
+
         System.out.println("입력하세요");
         String line=s.nextLine();
 
-        String temp[]=new String [10];
+        String temp[];
+        ArrayList temp_number=new ArrayList();
+        ArrayList temp_word=new ArrayList();
+
 
         temp=line.split(" ");
 
         for(int i = 0; i<temp.length; i++){
-            System.out.println(temp[i]);
+
+            if(Character.isDigit(temp[i].charAt(0))==false){
+                word.add(temp[i]);
+            }else {
+                number.add(temp[i]);
+            }
+
+
+            temp_number.add(number.poll());
+            temp_word.add(word.poll());
+
         }
-
-       /* Queue<Integer> number=new LinkedList<>();
-        Queue<String> word=new LinkedList<>();
-
+        temp_number.removeAll(Collections.singletonList(null));
+        temp_word.removeAll(Collections.singletonList(null));
 
 
-        try{
-            number.offer(s.nextInt());
-            word.offer(s.next());
-        }catch (InputMismatchException e){
-            System.out.println("입력이 잘못 되었습니다.");
+        System.out.println(temp_number);
+        System.out.println(temp_word);
+        System.out.println(temp_number.get(0));
+
+        int result= (int)temp_number.get(0);;
+
+        for (int i=1;i<temp_word.size();i++){
+
+
+
+            if(temp_word.get(i).equals("+")){
+                result=result+(int)temp_number.get(i);
+
+            }else if(temp_word.get(i).equals("-")) {
+                result=result-(int)temp_number.get(i);
+
+            }else if(temp_word.get(i).equals("/")) {
+                result=result/(int)temp_number.get(i);
+            }else if(temp_word.get(i).equals("*")){
+                result=result*(int)temp_number.get(i);
+            }
         }
-
-
-        if(word.poll().equals("+")){
-
-        }else if(word.poll().equals("-")) {
-
-        }else if(word.poll().equals("/")){
-
-        }else if(word.poll().equals("*")){
-
-        }else {
-            System.out.println("error");
-        }*/
+        System.out.println(result);
 
     }
 }
