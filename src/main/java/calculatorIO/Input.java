66837dd1917300e89207a6f43e5716b63dfc10fe
LSH-3016 <a673813@naver.com>
2021-11-25 23:25:21 +0900

    create method

--- src/main/java/calculatorIO/Input.java
+++ src/main/java/calculatorIO/Input.java
@@ -1,4 +1,18 @@
 package calculatorIO;
 
+import java.util.Scanner;
+
 public class Input {
+    private final String INPUT_MESSAGE = "식을 입력해주세요 : ";
+    private final Scanner SCANNER = new Scanner(System.in);
+
+
+    public Input(){
+
+    }
+
+    private String inputData(){
+        System.out.print(INPUT_MESSAGE);
+        return SCANNER.nextLine();
+    }
 }
