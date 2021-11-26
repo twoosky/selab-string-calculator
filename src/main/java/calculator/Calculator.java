82c8b75b92e71223988b1ff89b00a353aac34138
LSH-3016 <a673813@naver.com>
2021-11-26 17:11:39 +0900

    식과 결과에 대한 메소드 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -5,11 +5,28 @@ import java.util.Queue;
 
 public class Calculator {
     public int result;
+    public String formula;
 
     public Queue<Number> number = new LinkedList<>();
     public Queue<Operator> operator = new LinkedList<>();
 
     public Calculator(){
 
+    }
+
+    public void setFormula(String formula){
+        this.formula = formula;
+    }
+
+    public String getFormula(){
+        return formula;
+    }
+
+    public void setResult(int result){
+        this.result = result;
+    }
+
+    public int getResult(){
+        return result;
     }
 }
