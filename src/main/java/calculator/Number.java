d2055f0332941c7be5d7f2eeb97bdd73538d1445
LSH-3016 <a673813@naver.com>
2021-11-26 17:14:49 +0900

    Number 객체 로직 작성

--- src/main/java/calculator/Number.java
+++ src/main/java/calculator/Number.java
@@ -1,7 +1,27 @@
 package calculator;
 
-public class Number {
-    public Number(){
+import calculatorIO.validity;
 
+public class Number implements validity {
+
+    private final int number;
+
+    public Number(String number){
+
+
+        this.number = Integer.parseInt(number);
+    }
+
+    public int getNumber(){
+        return this.number;
+    }
+
+    @Override
+    public void doValidation(String data) {
+        try{
+            Integer.parseInt(data);
+        } catch(Exception e){
+
+        }
     }
 }
