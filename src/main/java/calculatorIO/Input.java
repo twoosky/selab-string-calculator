5443d212343cdd145662194dadb1fc21e9a1b8c8
LSH-3016 <a673813@naver.com>
2021-11-26 17:13:54 +0900

    생성 동작과 데이터입력 관련 로직 수정

--- src/main/java/calculatorIO/Input.java
+++ src/main/java/calculatorIO/Input.java
@@ -1,18 +1,30 @@
 package calculatorIO;
 
+import calculator.Calculator;
+
 import java.util.Scanner;
 
 public class Input {
     private final String INPUT_MESSAGE = "식을 입력해주세요 : ";
     private final Scanner SCANNER = new Scanner(System.in);
+    private final String separator = " ";
 
-
-    public Input(){
+    public Input(Calculator calculator){
+        calculator.setFormula(getData());
+        String[] splitedFormula = split(calculator.getFormula());
 
     }
 
-    private String inputData(){
+    private void printInputMessage(){
         System.out.print(INPUT_MESSAGE);
+    }
+
+    private String getData(){
+        printInputMessage();
         return SCANNER.nextLine();
+    }
+
+    private String[] split(String formula){
+        return formula.split(separator);
     }
 }
