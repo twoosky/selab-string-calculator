9fac9770ac043cc17d258f30b52f53f60430b709
LSH-3016 <a673813@naver.com>
2021-11-26 17:15:12 +0900

    실행 여부 확인을 위한 예시 코드 작성

--- src/main/java/calculator/play.java
+++ src/main/java/calculator/play.java
@@ -1,7 +1,12 @@
 package calculator;
 
+import calculatorIO.Input;
+
 public class play {
     public static void main (String[] args){
+        Calculator calculator = new Calculator();
+        Input input = new Input(calculator);
 
+        System.out.print(calculator.formula);
     }
 }
