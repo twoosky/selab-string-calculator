9c4f9d08f93a05ad8f38f8ad036d08a690d0268e
yunsup <zksdbstjq@gmail.com>
2021-12-05 16:15:19 +0900

    Docs : 변수명 카멜케이스 적용

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class Input {
 
-  private final static Scanner SCANNER = new Scanner(System.in);
+  private final static Scanner scanner = new Scanner(System.in);
   private final static String REGEX = " ";
 
   protected List<String> scanLine() {
     System.out.println("식을 입력해주세요");
-    return new ArrayList<>(Arrays.asList(SCANNER.nextLine().split(REGEX)));
+    return new ArrayList<>(Arrays.asList(scanner.nextLine().split(REGEX)));
   }
 }
