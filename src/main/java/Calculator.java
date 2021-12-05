180f2a7a1b337dee6f4b139a4499129a9555cb2a
yunsup <zksdbstjq@gmail.com>
2021-12-05 16:10:39 +0900

    Fix : Operators Enum 구현으로 불필요 구문 삭제

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,9 +1,7 @@
-import Operators.*;
 import java.util.*;
 
 public class Calculator {
 
-  private final static Progress PROGRESS = new Progress();
   private final static int FRONT_NUMBER = 0;
   private final static int OPERATOR_INDEX = 1;
   private final static int BEHIND_NUMBER = 2;
@@ -14,21 +12,8 @@ public class Calculator {
       String operator = inputs.get(OPERATOR_INDEX);
       int frontNumber = Integer.parseInt(inputs.get(FRONT_NUMBER));
       int behindNumber = Integer.parseInt(inputs.get(BEHIND_NUMBER));
+      answer = Operators.calculating(frontNumber, operator, behindNumber);
 
-      switch (operator) {
-        case "+":
-          answer = PROGRESS.calculating(new Plus(), frontNumber, behindNumber);
-          break;
-        case "-":
-          answer = PROGRESS.calculating(new Minus(), frontNumber, behindNumber);
-          break;
-        case "*":
-          answer = PROGRESS.calculating(new Multiple(), frontNumber, behindNumber);
-          break;
-        case "/":
-          answer = PROGRESS.calculating(new Divide(), frontNumber, behindNumber);
-          break;
-      }
       removeUsedIndex(inputs);
 
       if (inputs.isEmpty()) {
