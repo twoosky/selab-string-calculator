c4fe989c762968c4f80bde17cf901947e2e4d7d6
yunsup <zksdbstjq@gmail.com>
2021-12-05 16:30:55 +0900

    Docs : 변수명 카멜케이스 적용

--- src/main/java/Operators/Progress.java
+++
@@ -1,8 +0,0 @@
-package Operators;
-
-public class Progress {
-
-  public int calculating(Operators operators, int frontNumber, int behindNumber) {
-    return operators.calculate(frontNumber, behindNumber);
-  }
-}
