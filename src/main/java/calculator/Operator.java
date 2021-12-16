e8be96da4718697380aad5bbfc488d140f123ce4
LSH-3016 <a673813@naver.com>
2021-12-16 14:33:33 +0900

    연산자 객체 생성 로직 및 유효성 검사 추가

--- src/main/java/calculator/Operator.java
+++ src/main/java/calculator/Operator.java
@@ -1,7 +1,37 @@
 package calculator;
 
-public class Operator {
-    public Operator(){
+import calculation.Plus;
+import calculatorIO.validity;
+import error.OperatorError;
 
+public class Operator implements validity {
+
+
+    private final String operator;
+
+    public Operator(Calculator calculator, String operator){
+        doValidation(calculator, operator);
+        this.operator = operator;
+        addOperator(calculator,this);
+    }
+
+    private void addOperator(Calculator calculator,Operator operator){
+        calculator.operator.add(operator);
+    }
+
+    public String getOperator(){
+        return this.operator;
+    }
+
+    @Override
+    public void doValidation(Calculator calculator, String data) {
+        try{
+            if(!(data.equals(calculator.PLUS.getSIGN()))  && !(data.equals(calculator.MINUS.getSIGN())) && !(data.equals(calculator.TIMES.getSIGN())) && !(data.equals(calculator.DIVISION.getSIGN()))){
+                throw new Exception();
+            }
+        }catch(Exception e){
+            new OperatorError().printErrorMessage();
+            System.exit(0);
+        }
     }
 }
