ed9cd67ebf3493bf988a64a75bcf4a0a0530b16a
LSH-3016 <a673813@naver.com>
2021-12-16 14:35:03 +0900

    계산 결과 출력 로직 작성

--- src/main/java/calculatorIO/Output.java
+++ src/main/java/calculatorIO/Output.java
@@ -1,4 +1,16 @@
 package calculatorIO;
 
+import calculator.Calculator;
+
 public class Output {
+    private final String OUTPUT_MESSAGE = "계산 결과 : ";
+
+    public Output(Calculator calculator){
+        printOutputMessage();
+        System.out.println(calculator.getResult());
+    }
+
+    public void printOutputMessage(){
+        System.out.print("\n" + OUTPUT_MESSAGE);
+    }
 }
