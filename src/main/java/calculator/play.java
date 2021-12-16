612bde7f80d8c80c8545b67000b23f7c61a10bdc
LSH-3016 <a673813@naver.com>
2021-12-16 14:34:19 +0900

    메인에서 계산기가 실행되도록 작성

--- src/main/java/calculator/play.java
+++ src/main/java/calculator/play.java
@@ -1,12 +1,7 @@
 package calculator;
 
-import calculatorIO.Input;
-
 public class play {
     public static void main (String[] args){
-        Calculator calculator = new Calculator();
-        Input input = new Input(calculator);
-
-        System.out.print(calculator.formula);
+        new Calculator();
     }
 }
