707060b150b4130f1292c520f52fa5551210c8b6
yunsup <zksdbstjq@gmail.com>
2022-01-08 20:10:14 +0900

    Feat :  onProgress 실행문 Stream으로 변환중 재귀문 해결하지 못함

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,22 +1,47 @@
+import java.sql.ResultSet;
+import java.sql.SQLOutput;
 import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Calculator {
 
-    private final static String REGEX = "^[0-9]+$";
+  private final static String REGEX = "^[0-9]+$";
 
   public int onProgress(List<String> inputs) {
+
     int calculatedResult = Integer.parseInt(inputs.get(0));
+    IntStream.range(0, inputs.size())
+        .filter(Calculator::isEvenIndex)
+        .peek(System.out::println)
+        .map(i -> calculate(inputs.get(i), calculatedResult, inputs.get(i + 1)))
+        .forEach(System.out::println);
+    return calculatedResult;
+/*    int calculatedResult = Integer.parseInt(inputs.get(0));
     for (int i = 1; i < inputs.size(); i += 2) {
       calculatedResult = calculate(inputs.get(i), calculatedResult, inputs.get(i + 1));
     }
-    return calculatedResult;
+    return calculatedResult;*/
+  }
+
+  private static boolean isEvenIndex(int i) {
+    return i % 2 != 0 && i > 0;
+  }
+
+  private static int getCalculatedResult(List<String> inputs) {
+    return Integer.parseInt(inputs.get(0));
+  }
+
+  private static int toLambda(List<String> inputs, int index, int calculatedResult) {
+    return calculate(inputs.get(index), calculatedResult, inputs.get(index + 1));
   }
 
   private static int calculate(String operator, int frontOperand, String behindOperand) {
     return Operator
         .verifySymbol(operator)
         .applyOperand(frontOperand, Integer.parseInt(behindOperand));
-
   }
+
 }
 
